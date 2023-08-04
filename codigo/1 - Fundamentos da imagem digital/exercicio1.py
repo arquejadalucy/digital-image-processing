8f17e763d0632a2b0c2fa3b5537ec5c10945f217
arquejadalucy <lucy.velasco@jusbrasil.com.br>
2023-08-04 20:40:50 -0300

    Revert "initial commit"
    This reverts commit 172ee74039f85288419e9e37fc5344310c0d0fec.

---
+++ codigo/1 - Fundamentos da imagem digital/exercicio1.py
@@ -0,0 +1,11 @@
+# 1) Crie uma imagem 400 x 400 e utilizando o opencv com o python a transforme num tabuleiro de xadrez aonde cada quadrado seja do tamanho 50x50.
+# 2)Crie uma imagem 300 x 300 e utilizando o opencv com o python a transforme num jogo da velha com o X e 0 aonde dê empate
+# Sabendo:
+#
+# cv2.line(imagem, ponto_inicial, ponto_final, cor, espessura)
+# cv2.rectangle(imagem, canto_superior_esquerdo, canto_inferior_direito, cor, espessura)
+# cv2.circle(imagem, ponto_centro, raio, cor, espessura)
+# Onde:
+# Ponto e canto é a coordenada do ponto(x,y)
+# Cor é (b, g, r)
+
