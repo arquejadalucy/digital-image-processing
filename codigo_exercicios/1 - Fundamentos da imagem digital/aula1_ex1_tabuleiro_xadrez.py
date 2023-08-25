ba480326267a2ca2e70a7a0ae7083623d2086209
arquejadalucy <lucy.velasco@jusbrasil.com.br>
2023-08-25 09:31:12 -0300

    new files

--- codigo_exercicios/1 - Fundamentos da imagem digital/aula1_ex1_tabuleiro_xadrez.py
+++ codigo_exercicios/1 - Fundamentos da imagem digital/aula1_ex1_tabuleiro_xadrez.py
@@ -27,14 +27,3 @@ print(img_array.shape)
 
 cv2.imshow('Tabuleiro xadrez', img_array)
 cv2.waitKey(0)
-
-# 2)Crie uma imagem 300 x 300 e utilizando o opencv com o python a transforme num jogo da velha com o X e 0 aonde dê empate
-# Sabendo:
-#
-# cv2.line(imagem, ponto_inicial, ponto_final, cor, espessura)
-# cv2.rectangle(imagem, canto_superior_esquerdo, canto_inferior_direito, cor, espessura)
-# cv2.circle(imagem, ponto_centro, raio, cor, espessura)
-# Onde:
-# Ponto e canto é a coordenada do ponto(x,y)
-# Cor é (b, g, r)
-
