f051157af2bcfe7c4aab30a39ace4ff1d4a3b6bf
arquejadalucy <lucy.velasco@jusbrasil.com.br>
2023-08-25 12:23:02 -0300

    ex2

--- codigo_exercicios/1 - Fundamentos da imagem digital/aula1_ex2_jogo_da_velha.py
+++ codigo_exercicios/1 - Fundamentos da imagem digital/aula1_ex2_jogo_da_velha.py
@@ -26,15 +26,41 @@ img_array = np.full(
     (image_height, image_width, number_of_color_channels),
     white, dtype=np.uint8)
 
-for i in [0, 100, 200]:
-    cv2.rectangle(img_array, [0+i, 0+i], [100+i, 100+i], black,
-                  thickness=1, lineType=cv2.LINE_4, shift=0)
-    cv2.rectangle(img_array, [0 + i, 0 + i], [200 + i, 200 + i], black,
-                  thickness=1, lineType=cv2.LINE_4, shift=0)
+for i in [100, 200]:
+    cv2.line(img_array, [i, 0], [i, 300], black,
+             thickness=3, lineType=cv2.LINE_4, shift=0)
+    cv2.line(img_array, [0, i], [300, i], black,
+             thickness=3, lineType=cv2.LINE_4, shift=0)
+
+
+for [i, j] in [[0, 0], [200, 0], [100, 0]]:
+    cv2.line(img_array, [i, j], [i + 100, j + 100], black,
+             thickness=1, lineType=cv2.LINE_4, shift=0)
+    cv2.line(img_array, [i + 100, j], [i, j + 100], black,
+             thickness=1, lineType=cv2.LINE_4, shift=0)
+#
+# cv2.line(img_array, [200, 0], [300, 100], black,
+#          thickness=1, lineType=cv2.LINE_4, shift=0)
+# cv2.line(img_array, [300, 0], [200, 100], black,
+#          thickness=1, lineType=cv2.LINE_4, shift=0)
+#
+# cv2.line(img_array, [100, 100], [200, 200], black,
+#          thickness=1, lineType=cv2.LINE_4, shift=0)
+# cv2.line(img_array, [100, 200], [200, 100], black,
+#          thickness=1, lineType=cv2.LINE_4, shift=0)
+
 """
 | x | 0 | x |
 | 0 | x | x |
 | 0 | x | 0 |
 """
+coordenadas_dos_x = {
+    1: [[0, 0], [100, 100], [100, 0], [0, 100]],
+    2: [[200, 0], [300, 100], [300, 0], [200, 100]],
+    3: [[100, 100], [200, 200], [100, 200], [200, 100]],
+    #4: [[i, j], [i+100, j+100], [j+100, j], [i, j+100]]
+    # 4: [[200,100], [300, 200], []]
+}
+
 cv2.imshow('Jogo da velha', img_array)
 cv2.waitKey(0)
