e6f1203eaf3d1c2271a6c121f3df108de9abf551
arquejadalucy <lucy.velasco@jusbrasil.com.br>
2023-08-25 09:46:30 -0300

    pep8 improvements

--- codigo_exercicios/1 - Fundamentos da imagem digital/aula1_ex1_tabuleiro_xadrez.py
+++ codigo_exercicios/1 - Fundamentos da imagem digital/aula1_ex1_tabuleiro_xadrez.py
@@ -8,23 +8,28 @@ import numpy as np
 image_height = 100
 image_width = 100
 number_of_color_channels = 3
-black = (0,0,0)
-white = (255,255,255)
+black = (0, 0, 0)
+white = (255, 255, 255)
 
 # Creating 100x100 black image
-img_array = np.full((image_height, image_width, number_of_color_channels), black, dtype=np.uint8)
+img_array = np.full(
+    (image_height, image_width, number_of_color_channels),
+    black, dtype=np.uint8)
 print(img_array.shape)
 print(img_array[int(image_width / 2)+3, int(image_height / 2)+3])
 # Creating the 50x50 white squares to obtain the chess board
-img_array[int(image_width / 2):image_width, int(image_height / 2):image_height] = white
+img_array[int(image_width / 2):image_width,
+          int(image_height / 2):image_height] = white
 img_array[0:int(image_width / 2), 0:int(image_height / 2)] = white
 print(img_array.shape)
 print(img_array[int(image_width / 2), int(image_height / 2)])
 print(img_array[int(image_width / 2)+3, int(image_height / 2)+3])
 
 # Repeat the array to obtain a 400x400 chess board
-img_array= np.concatenate((img_array, img_array, img_array, img_array), axis=0)
-img_array= np.concatenate((img_array, img_array, img_array, img_array), axis=1)
+img_array = np.concatenate(
+    (img_array, img_array, img_array, img_array), axis=0)
+img_array = np.concatenate(
+    (img_array, img_array, img_array, img_array), axis=1)
 print(img_array.shape)
 
 cv2.imshow('Tabuleiro xadrez', img_array)
--- codigo_exercicios/1 - Fundamentos da imagem digital/aula1_ex2_jogo_da_velha.py
+++ codigo_exercicios/1 - Fundamentos da imagem digital/aula1_ex2_jogo_da_velha.py
@@ -4,8 +4,9 @@ a transforme num jogo da velha com o X e 0 aonde dê empate
 Sabendo:
 
 cv2.line(imagem, ponto_inicial, ponto_final, cor, espessura)
-cv2.rectangle(imagem, canto_superior_esquerdo, canto_inferior_direito, cor, espessura)
 cv2.circle(imagem, ponto_centro, raio, cor, espessura)
+cv2.rectangle(imagem, canto_superior_esquerdo,
+              canto_inferior_direito, cor, espessura)
 Onde:
 Ponto e canto é a coordenada do ponto(x,y)
 Cor é (b, g, r)
@@ -17,5 +18,5 @@ import numpy as np
 image_height = 100
 image_width = 100
 number_of_color_channels = 3
-black = (0,0,0)
-white = (255,255,255)
+black = (0, 0, 0)
+white = (255, 255, 255)
